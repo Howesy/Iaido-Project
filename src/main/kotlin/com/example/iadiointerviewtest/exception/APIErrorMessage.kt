83857a9a27a279adf0c7056eac3c0c65d6454bbe
Charlie <howesybusiness@gmail.com>
2023-10-16 23:23:38 +0100

    Commented APIErrorMessage

--- src/main/kotlin/com/example/iadiointerviewtest/exception/APIErrorMessage.kt
+++ src/main/kotlin/com/example/iadiointerviewtest/exception/APIErrorMessage.kt
@@ -1,5 +1,11 @@
 package com.example.iadiointerviewtest.exception
 
+/**
+ * Constructor class for Exception error message.
+ * @param statusCode Status code of the exception.
+ * @param message Message of the exception.
+ */
+
 class APIErrorMessage(
     val statusCode: Int? = null,
     val message: String? = null
