3096ff4d49f649f72c7bf35a81b842d8a1daa4f1
Charlie <howesybusiness@gmail.com>
2023-10-16 19:39:36 +0100

    Overhauled Person service to implement custom API error handling.

--- src/main/kotlin/com/example/iadiointerviewtest/PersonService.kt
+++ src/main/kotlin/com/example/iadiointerviewtest/PersonService.kt
@@ -1,33 +1,36 @@
 package com.example.iadiointerviewtest
 
 import org.springframework.stereotype.Service
-import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 
 @Service
 class PersonService(private val personRepository: PersonRepository) {
 
-    //CRUD - Create
+    fun createPerson(person: Person): Person {
+        if (personRepository.findUniqueUsername(person.username) != null)
+            throw PersonAlreadyExists("ERROR: Unable to create that person, a person already exists with that username!")
 
-    fun createPerson(person: Person): Person = personRepository.save(person)
-
-    //CRUD - Retrieve
+        return personRepository.save(person)
+    }
 
     fun retrieveAllPersons():
             List<Person> = personRepository.findAll()
-
 
     fun retrieveAllPersonsByName(personName: String):
             List<Person> = personRepository.findByPersonName(personName)
 
     fun retrieveAllPersonsByAge(personAge: String):
-            List<Person> = personRepository.findAll(Sort.by(Sort.Direction.DESC, "age"))
+            List<Person> = personRepository.findByPersonAge(personAge)
+
+    fun retrieveAllPersonsByIdentifiers(personName: String?, personAge: String?):
+            List<Person> = personRepository.findPersonsByIdentifiers(personName, personAge)
 
     fun retrievePerson(personID: Long):
             Person = personRepository.findById(personID)
-            .orElseThrow{PersonNotFoundException(HttpStatus.NOT_FOUND, "Unable to retrieve person matching that ID, they do not exist.")}
+            .orElseThrow{PersonNotFound("ERROR: Unable to retrieve person matching that ID, they do not exist.")}
 
-    //CRUD - Update
+    fun retrievePersonByUnique(username: String):
+            Person? = personRepository.findUniqueUsername(username)
 
     fun updatePerson(personID: Long, person: Person): Person {
         if (personRepository.existsById(personID)) {
@@ -44,20 +47,14 @@ class PersonService(private val personRepository: PersonRepository) {
                     password = person.password
             )
 
-
-
             return personRepository.save(newPerson)
-        } else throw PersonNotFoundException(HttpStatus.NOT_FOUND, "Unable to retrieve person matching that ID, they do not exist.")
+        } else throw PersonNotFound("ERROR: Unable to update person matching that ID, they do not exist.")
     }
-
-    //CRUD - Delete
 
     fun deletePerson(personID: Long): Unit {
         if (personRepository.existsById(personID)) {
             return personRepository.deleteById(personID)
-        } else throw PersonNotFoundException(HttpStatus.NOT_FOUND, "Unable to retrieve person matching that ID, they do not exist.")
+        } else throw PersonNotFound("ERROR: Unable to delete person matching that ID, they do not exist.")
     }
 
 }
-
-class PersonNotFoundException(httpStatus: HttpStatus, message: String): Exception("Error Code: ${httpStatus.toString()} - $message")
