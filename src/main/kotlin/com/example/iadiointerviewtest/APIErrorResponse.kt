a12516648a688aa4fa7053b8c0929ea18e3ad0f6
Charlie <howesybusiness@gmail.com>
2023-10-16 21:35:36 +0100

    Implemented exception handling for missing parameters and states

--- src/main/kotlin/com/example/iadiointerviewtest/APIErrorResponse.kt
+++ src/main/kotlin/com/example/iadiointerviewtest/APIErrorResponse.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.MissingServletRequestParameterException
 
 @ControllerAdvice
 class APIErrorResponse {
@@ -15,6 +16,18 @@ class APIErrorResponse {
 
     @ExceptionHandler
     fun handlePersonNotFound(exception: PersonNotFound): ResponseEntity<APIErrorMessage> {
+        val errorMessage = APIErrorMessage(HttpStatus.BAD_REQUEST.value(), exception.message)
+        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler
+    fun handleParameterException(exception: MissingServletRequestParameterException): ResponseEntity<APIErrorMessage> {
+        val errorMessage = APIErrorMessage(HttpStatus.BAD_REQUEST.value(), "ERROR: Missing parameter - ${exception.message}")
+        return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler
+    fun handleIllegalStateException(exception: IllegalStateException): ResponseEntity<APIErrorMessage> {
         val errorMessage = APIErrorMessage(HttpStatus.BAD_REQUEST.value(), exception.message)
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
