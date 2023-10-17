9b1e0d20fb6ed66a970b7eb0526deec0c742c8f9
Charlie <howesybusiness@gmail.com>
2023-10-17 14:12:41 +0100

    Commented "APIErrorResponse"

--- src/main/kotlin/com/example/iadiointerviewtest/exception/APIErrorResponse.kt
+++ src/main/kotlin/com/example/iadiointerviewtest/exception/APIErrorResponse.kt
@@ -8,11 +8,24 @@ import org.springframework.web.bind.MissingServletRequestParameterException
 
 @ControllerAdvice
 class APIErrorResponse {
+
+    /**
+     * Exception to be thrown when a person already exists in the database.
+     * @param exception PersonAlreadyExists exception
+     * @return ResponseEntity Sends error message to the API
+     */
+
     @ExceptionHandler
     fun handlePersonAlreadyExists(exception: PersonAlreadyExists): ResponseEntity<APIErrorMessage> {
         val errorMessage = APIErrorMessage(HttpStatus.BAD_REQUEST.value(), exception.message)
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
+
+    /**
+     * Exception to be thrown when a person cannot be found in the database.
+     * @param exception PersonNotFound exception
+     * @return ResponseEntity Sends error message to the API
+     */
 
     @ExceptionHandler
     fun handlePersonNotFound(exception: PersonNotFound): ResponseEntity<APIErrorMessage> {
@@ -20,11 +33,23 @@ class APIErrorResponse {
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
 
+    /**
+     * Exception to be thrown when a parameter isn't provided in the API
+     * @param exception MissingServletRequestParameterException exception
+     * @return ResponseEntity Sends error message to the API
+     */
+
     @ExceptionHandler
     fun handleParameterException(exception: MissingServletRequestParameterException): ResponseEntity<APIErrorMessage> {
         val errorMessage = APIErrorMessage(HttpStatus.BAD_REQUEST.value(), "ERROR: Missing parameter - ${exception.message}")
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
+
+    /**
+     * Illegal state exception handler
+     * @param exception IllegalStateException exception
+     * @return ResponseEntity Sends error message to the API
+     */
 
     @ExceptionHandler
     fun handleIllegalStateException(exception: IllegalStateException): ResponseEntity<APIErrorMessage> {
@@ -32,11 +57,23 @@ class APIErrorResponse {
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
 
+    /**
+     * General exception handler
+     * @param exception Exception exception
+     * @return ResponseEntity Sends error message to the API
+     */
+
     @ExceptionHandler
     fun handleGeneralException(exception: Exception): ResponseEntity<APIErrorMessage> {
         val errorMessage = APIErrorMessage(HttpStatus.BAD_REQUEST.value(), exception.message)
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
+
+    /**
+     * Exception to be thrown when illegal age data is provided to the API
+     * @param exception IllegalAgeConversion exception
+     * @return ResponseEntity Sends error message to the API
+     */
 
     @ExceptionHandler
     fun handleIllegalAgeConversion(exception: IllegalAgeConversion): ResponseEntity<APIErrorMessage> {
@@ -44,6 +81,8 @@ class APIErrorResponse {
         return ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST)
     }
 }
+
+//Classes for my custom exceptions
 
 class PersonAlreadyExists(message: String): RuntimeException(message) {}
 class PersonNotFound(message: String): RuntimeException(message) {}
